6847ad5528a09c368fe3e6c008a0dac65a98a60c
SHilditch4177 <s.j.hilditch@outlook.com>
2024-09-01 09:59:31 +0100

    updated

--- key_code.js
+++ key_code.js
@@ -1,4 +1,12 @@
-
-// left row 
-
-// center box
+const message = document.getElementById('message');
+document.addEventListener ("keypress", (event) =>{
+    message.innerText = ` ${event.key}`;
+});
+const code = document.getElementById("code");
+document.addEventListener ("keypress", (event) =>{
+    code.innerText = `key pressed: ${event.code}`;
+});
+const which = document.getElementById("which");
+document.addEventListener ("keypress", (event) =>{
+    which.innerText = `code: ${event.which}`;
+});
